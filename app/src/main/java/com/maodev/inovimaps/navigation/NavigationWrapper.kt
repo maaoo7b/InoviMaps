88dd17402dec121eca02fbaa9fabf4673774cd64
MaoDev <bmauricio.cruz07@gmail.com>
2025-10-14 21:50:22 -0500

    Created navigation wrapper to navigate from login screen to mapscreen and created the screens definitions.

---
+++ app/src/main/java/com/maodev/inovimaps/navigation/NavigationWrapper.kt
@@ -0,0 +1,28 @@
+package com.maodev.inovimaps.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.maodev.inovimaps.screens.login.LoginScreen
+import com.maodev.inovimaps.screens.map.MapScreen
+
+@Composable
+fun NavigationWrapper() {
+    val navController = rememberNavController()
+    val selectedDate by rememberSaveable { mutableStateOf("") }
+    NavHost(navController = navController, startDestination = LoginScreen) {
+        composable<LoginScreen> {
+            LoginScreen(
+                navigateToMapScreen = { navController.navigate(MapScreen) }
+            )
+        }
+        composable<MapScreen> {
+            MapScreen()
+        }
+    }
+
+}
---
+++ app/src/main/java/com/maodev/inovimaps/navigation/Screens.kt
@@ -0,0 +1,9 @@
+package com.maodev.inovimaps.navigation
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+object LoginScreen
+
+@Serializable
+object MapScreen
