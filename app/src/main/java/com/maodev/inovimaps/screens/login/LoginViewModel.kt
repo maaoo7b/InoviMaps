7d8203dfa038a46af38323f8c18d3f07cc980bee
MaoDev <bmauricio.cruz07@gmail.com>
2025-10-15 13:01:22 -0500

    Added function to send parameters in POST method and checks if parameters (email,password) are ok.

--- app/src/main/java/com/maodev/inovimaps/screens/login/LoginViewModel.kt
+++ app/src/main/java/com/maodev/inovimaps/screens/login/LoginViewModel.kt
@@ -1,9 +1,13 @@
 package com.maodev.inovimaps.screens.login
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.maodev.inovimaps.model.loginRequest
+import com.maodev.inovimaps.retrofit.InoviMapsRetrofitInstance
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 
 class LoginViewModel : ViewModel() {
 
@@ -17,10 +21,45 @@ class LoginViewModel : ViewModel() {
     fun onPasswordChanged(password: String) {
         _uiState.update { state -> state.copy(password = password) }
     }
+
+    fun loginUser() {
+        val email = _uiState.value.email
+        val password = _uiState.value.password
+        viewModelScope.launch {
+            try {
+                _uiState.update { it.copy(isLoading = true, message = null, loginSuccess = false) }
+
+                val response = InoviMapsRetrofitInstance.api.login(
+                    loginRequest(email = email, password = password)
+                )
+
+                _uiState.update {
+                    it.copy(
+                        isLoading = false,
+                        message = response.message,
+                        loginSuccess = response.message.contains("éxitoso", ignoreCase = true) // 👈 Aquí marcamos éxito
+                    )
+                }
+
+            } catch (e: Exception) {
+                _uiState.update {
+                    it.copy(
+                        isLoading = false,
+                        message = "Error ${e.message}",
+                        loginSuccess = false
+                    )
+                }
+            }
+        }
+    }
+
 }
 
 
 data class LoginUiState(
     val email: String = "",
-    val password: String = ""
+    val password: String = "",
+    val isLoading: Boolean = false,
+    val message: String? = null,
+    val loginSuccess: Boolean = false
 )
