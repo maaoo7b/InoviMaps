276e6d5083a56aad53ad79165e9b1888d3267aa0
MaoDev <bmauricio.cruz07@gmail.com>
2025-10-14 21:47:15 -0500

    Added the standard colors for the app.

--- app/src/main/java/com/maodev/inovimaps/ui/theme/Color.kt
+++ app/src/main/java/com/maodev/inovimaps/ui/theme/Color.kt
@@ -9,3 +9,7 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val Gray50 = Color(0XFF282C34)
+val White100 = Color(0xFFFFFFFF)
+val Blue80 = Color(0xFF01B0AF)
