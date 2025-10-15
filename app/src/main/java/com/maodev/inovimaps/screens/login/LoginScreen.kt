67665e3c88a7d1b059ebc60caf394b3e38bb97fa
MaoDev <bmauricio.cruz07@gmail.com>
2025-10-15 13:05:33 -0500

    Added launchedEffects to navigate when the credentials are ok.

--- app/src/main/java/com/maodev/inovimaps/screens/login/LoginScreen.kt
+++ app/src/main/java/com/maodev/inovimaps/screens/login/LoginScreen.kt
@@ -1,19 +1,25 @@
 package com.maodev.inovimaps.screens.login
 
+import android.util.Log
+import android.widget.Toast
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -31,7 +37,27 @@ fun LoginScreen(
     navigateToMapScreen: () -> Unit,
     loginViewModel: LoginViewModel = viewModel(),
 ) {
+    val context = LocalContext.current
     val uiState by loginViewModel.uiState.collectAsStateWithLifecycle()
+
+    Log.d("LoginScreen", "LaunchedEffect triggered: ${uiState.loginSuccess}")
+    Log.d("LoginScreen2", "loginSuccess=${uiState.loginSuccess}, message=${uiState.message}")
+
+    LaunchedEffect(uiState.loginSuccess) {
+        if (uiState.loginSuccess) {
+            Toast.makeText(context, "Inicio de sesión éxitoso", Toast.LENGTH_SHORT).show()
+            navigateToMapScreen()
+        }
+    }
+
+    LaunchedEffect(uiState.message) {
+        uiState.message?.let {
+            if (!uiState.loginSuccess) {
+                Toast.makeText(context, "Credenciales inválidas!", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
     Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
         Column(
             modifier = Modifier
@@ -61,13 +87,25 @@ fun LoginScreen(
             Spacer(modifier = Modifier.height(15.dp))
             Button(
                 modifier = Modifier.padding(vertical = 12.dp),
-                onClick = { navigateToMapScreen() },
+                onClick = {
+                    loginViewModel.loginUser()
+                    //navigateToMapScreen()
+                },
+                enabled = !uiState.isLoading,
                 shape = MaterialTheme.shapes.extraSmall
             ) {
-                InoviMapText(
-                    text = stringResource(R.string.login_screen_text_button),
-                    textStyle = MaterialTheme.typography.bodyLarge
-                )
+                if (uiState.isLoading) {
+                    CircularProgressIndicator(
+                        modifier = Modifier.size(22.dp),
+                        color = MaterialTheme.colorScheme.onPrimary,
+                        strokeWidth = 2.dp
+                    )
+                } else {
+                    InoviMapText(
+                        text = stringResource(R.string.login_screen_text_button),
+                        textStyle = MaterialTheme.typography.bodyLarge
+                    )
+                }
             }
         }
 
